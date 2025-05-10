618be8cbceca916aeba9fb832f504cbc930fc6d8
altr-r <arafatrahman0012@gmail.com>
2025-05-10 16:05:09 +0600

    news filtered

--- src/pages/CategoryNews.jsx
+++ src/pages/CategoryNews.jsx
@@ -1,9 +1,29 @@
-import React from "react";
-import { useParams } from "react-router";
+import React, { useState } from "react";
+import { useLoaderData, useParams } from "react-router";
+import { useEffect } from "react";
 
 const CategoryNews = () => {
-  const {id} = useParams();
-  return <div>This is Category News - {id}</div>;
+  const { id } = useParams();
+  const data = useLoaderData();
+
+  const [categoryNews, setCategoryNews] = useState([]);
+  useEffect(() => {
+    if (id === "0") {
+      setCategoryNews(data);
+      return;
+    } else if (id === "1") {
+      const filertedNews = data.filter(
+        (news) => news.others.is_today_pick == true
+      );
+      setCategoryNews(filertedNews);
+    } else {
+      const filertedNews = data.filter(
+        (news) => news.category_id === parseInt(id)
+      );
+      setCategoryNews(filertedNews);
+    }
+  }, [data, id]);
+  return <div>Total {categoryNews.length} news Found</div>;
 };
 
 export default CategoryNews;
