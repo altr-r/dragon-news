3ef153ec217bea18f115cf389046a35181e33835
altr-r <arafatrahman0012@gmail.com>
2025-05-10 13:34:35 +0600

    date implemented

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import logo from "/logo.png";
+import { format } from "date-fns";
 
 const Header = () => {
   return (
     <div className="flex flex-col justify-center items-center gap-3 py-4">
       <img src={logo} alt="" />
-      <p>Journalism without Fear or Favour</p>
+      <p className="text-accent">Journalism without Fear or Favour</p>
+      <div className="flex">
+        <p className="text-semibold">{format(new Date(), "EEEE")}</p>
+        <p className="text-accent">{format(new Date(), ", MMMM MM, yyyy")}</p>
+      </div>
     </div>
   );
 };
